f7015c282098319dc93098896df1e026ceadeb36
Hot Tamale <zweakland@setonhill.edu>
2021-03-09 23:53:44 -0500

    Final version

--- InClassSegues/ViewController.swift
+++ InClassSegues/ViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+// global variable
+var name: String = ""
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    @IBAction func nextPage(_ sender: Any) {
+        // store the input in name variable
+        name = nameField.text!
+        performSegue(withIdentifier: "segueNext", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
